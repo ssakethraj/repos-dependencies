6544ebb6d7f9a4da80b10d742dce1d699d42d745
ssakethraj <ssakethraj@gmail.com>
2019-06-20 23:56:43 +0530

    add fetch repo content action creator and reducer

--- src/redux/github/actions.js
+++ src/redux/github/actions.js
@@ -1,36 +1,76 @@
 export const actions = {
   FETCH_REPOS_REQUEST: "FETCH_REPOS_REQUEST",
   FETCH_REPOS_SUCCESS: "FETCH_REPOS_SUCCESS",
-  FETCH_REPOS_ERROR: "FETCH_REPOS_ERROR"
+  FETCH_REPOS_ERROR: "FETCH_REPOS_ERROR",
+  FETCH_CONTENT_REQUEST: "FETCH_CONTENT_REQUEST",
+  FETCH_CONTENT_SUCCESS: "FETCH_CONTENT_SUCCESS",
+  FETCH_CONTENT_ERROR: "FETCH_CONTENT_ERROR"
 };
 
-const fetchStarted = () => {
+// action creators for fetch repos
+const fetchRepoStarted = () => {
   return {
     type: actions.FETCH_REPOS_REQUEST
   };
 };
-const fetchSuccess = payload => {
+const fetchRepoSuccess = payload => {
   return {
     type: actions.FETCH_REPOS_SUCCESS,
     payload
   };
 };
-const fetchFailure = err => {
+const fetchRepoFailure = err => {
   return {
-    type: actions.FETCH_REPOS_REQUEST,
+    type: actions.FETCH_REPOS_ERROR,
     error: err
   };
 };
 
+//action creators for fetch content
+const fetchContentStarted = () => {
+  return {
+    type: actions.FETCH_CONTENT_REQUEST
+  };
+};
+const fetchContentSuccess = payload => {
+  return {
+    type: actions.FETCH_CONTENT_SUCCESS,
+    payload
+  };
+};
+const fetchContentFailure = err => {
+  return {
+    type: actions.FETCH_CONTENT_ERROR,
+    error: err
+  };
+};
+
+// FETCH REPOSITORY
 export const fetchRepos = () => dispatch => {
-  dispatch(fetchStarted());
+  dispatch(fetchRepoStarted());
   fetch("https://api.github.com/repositories")
     .then(res => res.json())
     .then(data => {
       console.log("Repos: ", data);
-      dispatch(fetchSuccess(data));
+      dispatch(fetchRepoSuccess(data));
     })
     .catch(err => {
-      dispatch(fetchFailure(err));
+      console.log("Error", err)
+      dispatch(fetchRepoFailure(err));
     });
 };
+
+//FETCH CONTENT
+export const fetchRepoContent = (username, reponame) => dispatch => {
+  console.log("triggered")
+  dispatch(fetchContentStarted());
+  fetch(`https://api.github.com/repos/${username}/${reponame}/contents/package.json`)
+    .then(res => res.json())
+    .then(data => {
+      console.log("Content: ", data);
+      dispatch(fetchContentSuccess(data));
+    })
+    .catch(err => {
+      dispatch(fetchContentFailure(err));
+    });
+}
--- src/redux/github/reducer.js
+++ src/redux/github/reducer.js
@@ -7,11 +7,39 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case actions.FETCH_REPOS_REQUEST:
-      return { ...state, loading: true };
+      return {
+        ...state,
+        loading: true
+      };
     case actions.FETCH_REPOS_SUCCESS:
-      return { ...state, loading: false, repos: action.payload };
+      return {
+        ...state,
+        loading: false,
+        repos: action.payload
+      };
     case actions.FETCH_REPOS_ERROR:
-      return { ...state, loading: false, error: action.err };
+      return {
+        ...state,
+        loading: false,
+        error: action.error
+      };
+    case actions.FETCH_CONTENT_REQUEST:
+      return {
+        ...state,
+        loading: true
+      };
+    case actions.FETCH_CONTENT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        content: action.payload
+      };
+    case actions.FETCH_CONTENT_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.error
+      };
     default:
       return state;
   }
