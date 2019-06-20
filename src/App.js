6f4c22028a5db26545e7e5c703a3100fb4899b40
ssakethraj <ssakethraj@gmail.com>
2019-06-20 22:53:47 +0530

    create react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Github from "./Containers/Github";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Github />
+    </div>
+  );
+}
+
+export default App;
